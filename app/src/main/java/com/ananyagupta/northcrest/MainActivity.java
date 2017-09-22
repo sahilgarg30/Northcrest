bc4ae3185a36b898915a4880504059523f39de37
Ananya <ananyagupta6@gmail.com>
2017-09-23 00:25:41 +0530

    Sign In UI and drawable images

--- app/src/main/java/com/ananyagupta/northcrest/MainActivity.java
+++ app/src/main/java/com/ananyagupta/northcrest/MainActivity.java
@@ -2,6 +2,7 @@ package com.ananyagupta.northcrest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
+        );
     }
 }
