993a4be9e3ff59b7228f3f1e01e6a661e7ceb6f9
sahilgarg30 <sahilgarg30@yahoo.co.in>
2017-09-22 17:46:48 +0530

    Fragments added

---
+++ app/src/main/java/com/ananyagupta/northcrest/HistoryFragment.java
@@ -0,0 +1,30 @@
+package com.ananyagupta.northcrest;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HistoryFragment extends Fragment {
+
+
+    public HistoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_history, container, false);
+        return view;
+    }
+
+}
---
+++ app/src/main/java/com/ananyagupta/northcrest/SupportFragment.java
@@ -0,0 +1,30 @@
+package com.ananyagupta.northcrest;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SupportFragment extends Fragment {
+
+
+    public SupportFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_support, container, false);
+        return view;
+    }
+
+}
