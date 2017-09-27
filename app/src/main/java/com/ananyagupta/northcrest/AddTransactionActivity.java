5574fc22b08ca936993faa97fad573a3d8b047fa
Ananya <ananyagupta6@gmail.com>
2017-09-27 21:17:32 +0530

    Balance update in transaction along with rewards

--- app/src/main/java/com/ananyagupta/northcrest/AddTransactionActivity.java
+++ app/src/main/java/com/ananyagupta/northcrest/AddTransactionActivity.java
@@ -1,13 +1,34 @@
 package com.ananyagupta.northcrest;
 
+import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AddTransactionActivity extends AppCompatActivity {
+
+    private EditText mCustomerEt;
+    private EditText mAmountEt;
+    private double amount;
+    private double rewards;
+    private final String Deal="NorthCrest Stores";
+    private MyHelper mMyHelper;
+    private SQLiteDatabase mdB;
+    private RadioButton mDebitBtn;
+    private RadioButton mCreditBtn;
+    private double bal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +38,12 @@ public class AddTransactionActivity extends AppCompatActivity {
         getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
         );
+        mCustomerEt = (EditText) findViewById(R.id.editText7);
+        mAmountEt = (EditText) findViewById(R.id.editText2);
+        mMyHelper = new MyHelper(AddTransactionActivity.this,"USERSDB",null,1);
+        mdB = mMyHelper.getWritableDatabase();
+        mDebitBtn = (RadioButton) findViewById(R.id.debit_rb);
+        mCreditBtn = (RadioButton) findViewById(R.id.credit_rb);
     }
 
     @Override
@@ -27,5 +54,37 @@ public class AddTransactionActivity extends AppCompatActivity {
                         break;
         }
         return true;
+    }
+
+    public void handleTransaction(View view) {
+        amount = Double.parseDouble(mAmountEt.getText().toString());
+        String customer = mCustomerEt.getText().toString();
+        if(customer.equals(Deal))
+        {
+            rewards = 1.5*amount/100;
+        }
+        else
+        {
+            rewards=amount/100;
+        }
+        String[] args = {FirebaseAuth.getInstance().getCurrentUser().getEmail()};
+        Cursor c = mdB.query("users", null, "email = ?", args, null, null, null);
+        if(c.moveToNext())
+        {
+            bal = Double.parseDouble(c.getString(7));
+            if(mDebitBtn.isChecked())
+            {
+                bal=bal-amount;
+            }
+            else
+            {
+                bal=bal+amount;
+            }
+        }
+        ContentValues cv= new ContentValues();
+        cv.put("balance",bal);
+        cv.put("rewards",rewards);
+        mdB.update("users",cv,"email="+c.getString(1),null);
+
     }
 }
